000099200c46dae1e5f64fa69370f07e45090415
Stephane Labine <stephanelabine@gmail.com>
2026-01-15 19:57:48 -0800

    Security: remove hardcoded key leftovers and ignore replacements file

--- app.config.js
+++ app.config.js
@@ -6,11 +6,11 @@ module.exports = ({ config }) => {
       ...(config.extra || {}),
       firebase: {
         apiKey: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
-        authDomain:"legacy-golf-dev.firebaseapp.com",
-        projectId:  "legacy-golf-dev",
-        storageBucket:  "legacy-golf-dev.firebasestorage.app",
+        authDomain: "legacy-golf-dev.firebaseapp.com",
+        projectId: "legacy-golf-dev",
+        storageBucket: "legacy-golf-dev.firebasestorage.app",
         messagingSenderId: "87329359611",
-        appId: "1:87329359611:web:36ea3311124453e980925e"
+        appId: "1:87329359611:web:36ea3311124453e980925e",
       },
     },
   };
