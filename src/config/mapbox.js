e15ad289b4fba2de32ff6b26f31175253d46bd0f
Stephane Labine <stephanelabine@gmail.com>
2026-03-05 19:43:00 -0800

    Hole Map: fix Mapbox token config key

--- src/config/mapbox.js
+++ src/config/mapbox.js
@@ -1,6 +1,6 @@
 import Constants from "expo-constants";
 
 export const MAPBOX_TOKEN =
-  Constants?.expoConfig?.extra?.MAPBOX_TOKEN ||
-  Constants?.manifest?.extra?.MAPBOX_TOKEN ||
+  Constants?.expoConfig?.extra?.mapboxToken ||
+  Constants?.manifest?.extra?.mapboxToken ||
   "";
